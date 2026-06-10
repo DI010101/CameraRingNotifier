df5eafe30eaa13f90fe6d30a448c160ec21ca76b
DI010101 <dici.handy@gmail.com>
2026-06-10 08:21:52 +0200

    Aktualisieren von build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,10 +27,6 @@ android {
         compose = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
-    }
-
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -47,18 +43,29 @@ dependencies {
         platform("androidx.compose:compose-bom:2025.01.01")
 
     implementation(composeBom)
-
     androidTestImplementation(composeBom)
 
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
 
-    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(
+        "androidx.lifecycle:lifecycle-runtime-ktx:2.9.0"
+    )
 
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation(
+        "androidx.activity:activity-compose:1.10.1"
+    )
 
-    implementation("androidx.compose.material3:material3")
+    implementation(
+        "androidx.compose.ui:ui"
+    )
+
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
 
     implementation(
         "androidx.navigation:navigation-compose:2.8.8"
@@ -74,6 +81,14 @@ dependencies {
 
     implementation(
         "androidx.lifecycle:lifecycle-runtime-compose:2.9.0"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0"
+    )
+
+    implementation(
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2"
     )
 
     implementation(
