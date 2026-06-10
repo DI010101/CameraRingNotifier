2c9fe2ff4bb93a3019e344623a14e278211af209
DI010101 <dici.handy@gmail.com>
2026-06-10 08:04:03 +0200

    Aktualisieren von MainActivity.kt

--- app/src/main/java/com/cameraring/notifier/MainActivity.kt
+++ app/src/main/java/com/cameraring/notifier/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.cameraring.notifier
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.app.NotificationManagerCompat
 import com.cameraring.notifier.ui.settings.SettingsScreen
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +17,41 @@ class MainActivity : ComponentActivity() {
     ) {
         super.onCreate(savedInstanceState)
 
+        requestOverlayPermission()
+
+        requestNotificationListenerPermission()
+
         setContent {
             SettingsScreen()
+        }
+    }
+
+    private fun requestOverlayPermission() {
+
+        if (!Settings.canDrawOverlays(this)) {
+
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+
+            startActivity(intent)
+        }
+    }
+
+    private fun requestNotificationListenerPermission() {
+
+        val enabledListeners =
+            NotificationManagerCompat
+                .getEnabledListenerPackages(this)
+
+        if (!enabledListeners.contains(packageName)) {
+
+            startActivity(
+                Intent(
+                    Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS
+                )
+            )
         }
     }
 }
